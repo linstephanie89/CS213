12e1aa7fd7edc9612d657cc0a088799bf4567990
linst <linstephanie89@gmail.com>
2023-03-29 16:01:50 -0400

    made some more files/changes

--- Project4/src/main/java/com/example/project4/ShopManager.java
+++ Project4/src/main/java/com/example/project4/ShopManager.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class ShopManager extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ShopManager.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ShopManager.class.getResource("store-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
