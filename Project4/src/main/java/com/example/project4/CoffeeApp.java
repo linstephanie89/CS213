275c9d4a3030ee6f532eb9f29cd0e955564461f8
michelleleeyim <102488766+michelleleeyim@users.noreply.github.com>
2023-04-02 19:53:46 -0400

    started doing coffee

--- Project4/src/main/java/com/example/project4/CoffeeApp.java
+++ Project4/src/main/java/com/example/project4/CoffeeApp.java
@@ -1,4 +1,24 @@
 package com.example.project4;
+import rucafe.*
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 public class CoffeeApp {
+    @FXML
+    private MenuButton coffeeSize;
+
+    private String size;
+    @FXML
+    public void selectedCoffeeSize(ActionEvent event) {
+        MenuItem item = (MenuItem) event.getSource();
+        size = item.getText();
+        coffeeSize.setText(size);
+    }
+
+    @FXML
+
+
+
 }
