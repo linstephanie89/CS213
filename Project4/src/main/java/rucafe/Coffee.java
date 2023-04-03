6f9ca773b518568a19453dfc947c17578f9c3497
michelleleeyim <102488766+michelleleeyim@users.noreply.github.com>
2023-04-02 20:31:15 -0400

    started doing coffee

--- Project4/src/main/java/rucafe/Coffee.java
+++ Project4/src/main/java/rucafe/Coffee.java
@@ -2,39 +2,56 @@ package rucafe;
 
 public class Coffee extends MenuItem {
     private String size;
-    private String[] addIns;
+    private boolean[] addIns = [false, false, false, false, false];
+    private int numOrders;
     private int addInCount;
     private int NONE = 0;
-    private int CAPACITY = 4;
-    private int GROWTH_RATE = 4;
+    private int CAPACITY = 5;
     private double ADD_IN_PRICE = 0.30;
     private double SHORT_PRICE = 1.89;
     private double TALL_PRICE = 2.29;
     private double GRANDE_PRICE = 2.69;
     private double VENTI_PRICE = 3.09;
+    private boolean addedIn = false;
 
 
-    public Coffee (String size, double price) {
+    public Coffee (String size, double price, int numOrders) {
         super(price);
         this.size = size;
         this.addIns = String[CAPACITY];
         this.addInCount = NONE;
-    }
-
-    private void grow(){
-        String[] oldList = this.addIns;
-        this.addIns = String[this.addInCount + GrowthRate];
-        for (int i = 0; i < this.addInCount; i++) {
-            this.addIns[i] = oldList[i];
-        }
+        this.numOrders = numOrders;
     }
     public void setAddInCount(int count) {
         addInCount += count;
     }
 
     public void setAddIns(String addIn) {
-        addIn[addInCount] = addIn;
+        addedIn = true;
+        if (addIn.equals("Sweet Cream")) {
+            addIns[0] = true;
+        } if (addIn.equals("French Vanilla")) {
+            addIns[1] = true;
+        } if (addIn.equals("Irish Cream")) {
+            addIns[2] = true;
+        } if (addIn.equals("Caramel")) {
+            addIns[3] = true;
+        } if (addIn.equals("Mocha")) {
+            addIns[4] = true;
+        }
         addInCount += 1;
+    }
+
+    private String getFlavor(int index) {
+        if (index == 0) {
+            return "Sweet Cream";
+        } else if (index == 1) {
+            return "French Vanila";
+        } else if (index == 2) {
+            return "Irish Cream";
+        } else if (index == 3) {
+            return "Caramel";
+        } return "Mocha";
     }
 
     @Override
@@ -52,6 +69,30 @@ public class Coffee extends MenuItem {
         return this.price;
     }
 
+    @Override
+    public String toString(){
+        boolean multipleFlavors = false;
+        String returnString = "";
+        if (numOrders == 1) {
+            returnString = "1 coffee";
+        }
+        returnString = Integer.toString(numOrders) + " coffees";
+        if (addedIn) {
+            returnString += " with";
+            for (int i = 0; i < 5; i++) {
+                if (multipleFlavors == true) {
+                    returnString += ",";
+                }
+                if (addIns[i] == true) {
+                    multipleFlavors = true;
+                    returnString += " " + getFlavor(i);
+
+                }
+            }
+        }
+        returnString += " ordered! Total price is " + Integer.toString(this.itemPrice()) +".";
+        return returnString;
+    }
 
 
 }
--- Project4/src/main/java/rucafe/Order.java
+++ Project4/src/main/java/rucafe/Order.java
@@ -2,4 +2,19 @@ package rucafe;
 
 public class Order {
     private MenuItem[] orderBasket;
+    private int EMPTY = 0;
+    Private int CAPACITY = 4;
+
+    public Order() {
+        this.orderBasket = new MenuItem[CAPACITY][];
+        this.size = EMPTY;
+    }
+
+    private void grow(){
+        String[] oldList = this.addIns;
+        this.addIns = String[this.addInCount + GrowthRate];
+        for (int i = 0; i < this.addInCount; i++) {
+            this.addIns[i] = oldList[i];
+        }
+    }
 }
